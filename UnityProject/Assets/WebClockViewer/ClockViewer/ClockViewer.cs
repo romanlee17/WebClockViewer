30ef8b5160238f935f8e748ff296689e85faf9fc
Roman Likhadievski <136405707+romanlee17@users.noreply.github.com>
2026-09-24 15:28:24 +0300

    add time labels

---
+++ UnityProject/Assets/WebClockViewer/ClockViewer/ClockViewer.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WebClockViewer
+{
+    internal class ClockViewer : MonoBehaviour
+    {
+        [SerializeField] private Text _utcTimeText;
+        [SerializeField] private Text _localTimeText;
+
+
+    }
+}
