dd4310a0b13bd6d6bac887e460eae388e81c609d
Roman Likhadievski <136405707+romanlee17@users.noreply.github.com>
2026-09-24 15:41:27 +0300

    fetch time from mirrasdk

--- UnityProject/Assets/WebClockViewer/ClockViewer/ClockViewer.cs
+++ UnityProject/Assets/WebClockViewer/ClockViewer/ClockViewer.cs
@@ -1,3 +1,6 @@
+using Cysharp.Threading.Tasks;
+using MirraGames.SDK;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,6 +11,23 @@ namespace WebClockViewer
         [SerializeField] private Text _utcTimeText;
         [SerializeField] private Text _localTimeText;
 
+        private async void Start()
+        {
+            await ClockUpdateTask();
+        }
 
+        private async UniTask ClockUpdateTask()
+        {
+            while (true)
+            {
+                DateTime currentDate = MirraSDK.Time.CurrentDate;
+                DateTime utcDate = currentDate.ToUniversalTime();
+
+                _utcTimeText.text = $"UTC+0: {utcDate:HH:mm:ss}";
+                _localTimeText.text = $"Local time: {currentDate:HH:mm:ss}";
+
+                await UniTask.Delay(1000);
+            }
+        }
     }
 }
